2b348f94df4fe36139cbdbb25e478ca3db7a3d05
Mohamed Hatem <mohamed.hatem157@example.com>
2024-09-04 19:50:27 +0300

    Add project files.

---
+++ G6.NetITIMVCDay01/Pages/StudentForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace G6.NetITIMVCDay01.Pages
+{
+    public class StudentFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
